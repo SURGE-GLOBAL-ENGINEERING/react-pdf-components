504c4189d2691399a9192a7d3801dab983c23e49
Sansith <seniya@the-ai.team>
2021-10-29 23:30:42 +0530

    fix(ci/cd):add lib version bump script

---
+++ .github/scripts/bump-version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit -o noclobber -o nounset -o pipefail
+
+# This script uses the parent version as the version to publish a library with
+
+getBuildType() {
+  local release_type="minor"
+  if [[ "$1" == *"feat"* ]]; then
+    release_type="major"
+  elif [[ "$1" == *"fix"* || "$1" == *"docs"* || "$1" == *"chore"* ]]; then
+    release_type="patch"
+  fi
+  echo "$release_type"
+}
+
+PARENT_DIR="$PWD"
+ROOT_DIR="."
+COMMIT_MESSAGE="$(git log -1 --pretty=format:"%s")"
+RELEASE_TYPE=${1:-$(getBuildType "$COMMIT_MESSAGE")}
+DRY_RUN=${DRY_RUN:-"True"}
+cd ./libs/react-pdf-components
+npm version "$RELEASE_TYPE"  -m "react-pdf-components $RELEASE_TYPE %s release"
