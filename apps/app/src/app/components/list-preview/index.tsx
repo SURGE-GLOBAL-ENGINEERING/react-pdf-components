e06199144830f6c7ecbd7ed09ad5168376d9538b
Nishan Wijethunga <nishansameerawijethunga@gmail.com>
2021-12-03 04:38:11 +0530

    fix(list-styles): Add examples for list

--- apps/app/src/app/components/list-preview/index.tsx
+++ apps/app/src/app/components/list-preview/index.tsx
@@ -9,8 +9,130 @@ import { FC } from 'react';
 const ListPreview: FC = () => {
   return (
     <Page wrap size="A4">
-      <List type="ol">
-        <ListItem fontSize={10}>
+      <List type="ol" style={{ fontFamily: 'EmilysCandy', fontSize: 11 }}>
+        <ListItem>
+          <TextNode>Part 1</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 2</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 3</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 4</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 5</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 6</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 7</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 8</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 9</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 10</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 11</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 12</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 13</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 14</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 15</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 16</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 17</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 18</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 19</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>Part 20</TextNode>
+          <List type="ol">
+            <ListItem>
+              <TextNode>Part 1</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 2</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 3</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 4</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 5</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 6</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 7</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 8</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 9</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 10</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 11</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 12</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 13</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 14</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 15</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 16</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 17</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 18</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 19</TextNode>
+            </ListItem>
+            <ListItem>
+              <TextNode>Part 20</TextNode>
+            </ListItem>
+          </List>
+        </ListItem>
+        <ListItem>
           <TextNode>chapter 1</TextNode>
         </ListItem>
         <ListItem>
@@ -44,7 +166,7 @@ const ListPreview: FC = () => {
                     </ListItem>
                     <ListItem>
                       <TextNode>section 2</TextNode>
-                      <List type="ul">
+                      <List type="ol">
                         <ListItem>
                           <TextNode>sub section 1</TextNode>
                         </ListItem>
@@ -103,6 +225,14 @@ const ListPreview: FC = () => {
         </ListItem>
         <ListItem>
           <TextNode>chapter 5</TextNode>
+        </ListItem>
+      </List>
+      <List type="ul">
+        <ListItem>
+          <TextNode>sub section 1</TextNode>
+        </ListItem>
+        <ListItem>
+          <TextNode>sub section 2</TextNode>
         </ListItem>
       </List>
     </Page>
