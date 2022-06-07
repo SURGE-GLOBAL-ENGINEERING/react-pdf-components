3b0166b2daa73f4ab576449b30076d0a6f4a5742
Sashika Nawarathne <hi@sashika.me>
2022-06-07 16:32:32 +0530

    fix(list-alignment): Fix list view layout issues

--- apps/app/src/app/components/list-preview/index.tsx
+++ apps/app/src/app/components/list-preview/index.tsx
@@ -3,257 +3,76 @@ import {
   ListItem,
   Page,
   TextNode,
+  View,
 } from '@paladin-analytics/react-pdf-components';
 import { FC } from 'react';
+
+const SIZE = 20;
+
+const lorem =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const ListPreview: FC = () => {
   return (
     <Page wrap size="A4">
-      <List type="ol" style={{ fontFamily: 'EmilysCandy', fontSize: 11 }}>
-        <ListItem>
-          <TextNode>Part 1</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 2</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 3</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 4</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 5</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 6</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 7</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 8</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 9</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 10</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 11</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 12</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 13</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 14</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 15</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 16</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 17</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 18</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 19</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>Part 20</TextNode>
-          <List type="ol">
+      <View
+        style={{
+          marginLeft: '100px',
+          marginRight: '100px',
+        }}
+      >
+        <List type="ol" style={{ fontSize: 11 }}>
+          {[...Array(SIZE)].map((x, i) => {
+            // if (i === 5) {
+            //   return (
+            //     <List type="ul">
+            //       {[...Array(SIZE / 2)].map((x, i) => {
+            //         if (i === 5) {
+            //           return (
+            //             <List type="ol">
+            //               {[...Array(SIZE / 2)].map((x) => {
+            //                 if (i === 5) {
+            //                   return (
+            //                     <ListItem>
+            //                       <TextNode>{lorem}</TextNode>
+            //                     </ListItem>
+            //                   );
+            //                 }
+            //                 return (
+            //                   <ListItem>
+            //                     <TextNode>{lorem}</TextNode>
+            //                   </ListItem>
+            //                 );
+            //               })}
+            //             </List>
+            //           );
+            //         }
+
+            //         return (
+            //           <ListItem>
+            //             <TextNode>{lorem}</TextNode>
+            //           </ListItem>
+            //         );
+            //       })}
+            //     </List>
+            //   );
+            // }
+
+            return (
+              <ListItem>
+                <TextNode>{lorem}</TextNode>
+              </ListItem>
+            );
+          })}
+        </List>
+        {/* <List type="ul" style={{ fontSize: 24 }}>
+          {[...Array(SIZE)].map((x) => (
             <ListItem>
-              <TextNode>Part 1</TextNode>
+              <TextNode>{lorem}</TextNode>
             </ListItem>
-            <ListItem>
-              <TextNode>Part 2</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 3</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 4</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 5</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 6</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 7</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 8</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 9</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 10</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 11</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 12</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 13</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 14</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 15</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 16</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 17</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 18</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 19</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>Part 20</TextNode>
-            </ListItem>
-          </List>
-        </ListItem>
-        <ListItem>
-          <TextNode>chapter 1</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>chapter 2</TextNode>
-          <List type="ol">
-            <ListItem>
-              <TextNode>section 1</TextNode>
-            </ListItem>
-          </List>
-        </ListItem>
-        <ListItem>
-          <TextNode>chapter 3</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>chapter 4</TextNode>
-          <List type="ol">
-            <ListItem>
-              <TextNode>section 1</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>section 2</TextNode>
-              <List type="ol">
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                  <List type="ol">
-                    <ListItem>
-                      <TextNode>section 1</TextNode>
-                    </ListItem>
-                    <ListItem>
-                      <TextNode>section 2</TextNode>
-                      <List type="ol">
-                        <ListItem>
-                          <TextNode>sub section 1</TextNode>
-                        </ListItem>
-                        <ListItem>
-                          <TextNode>sub section 2</TextNode>
-                        </ListItem>
-                      </List>
-                    </ListItem>
-                    <ListItem>
-                      <TextNode>section 3</TextNode>
-                    </ListItem>
-                  </List>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section x</TextNode>
-                </ListItem>
-              </List>
-            </ListItem>
-          </List>
-        </ListItem>
-        <ListItem>
-          <TextNode>chapter 5</TextNode>
-        </ListItem>
-      </List>
-      <List type="ul" style={{ fontFamily: 'EmilysCandy', fontSize: 11 }}>
-        <ListItem>
-          <TextNode>sub section 1</TextNode>
-        </ListItem>
-        <ListItem>
-          <TextNode>sub section 2</TextNode>
-          <List type="ul" style={{ fontFamily: 'EmilysCandy', fontSize: 11 }}>
-            <ListItem>
-              <TextNode>sub section 1</TextNode>
-            </ListItem>
-            <ListItem>
-              <TextNode>sub section 2</TextNode>
-              <List
-                type="ul"
-                style={{ fontFamily: 'EmilysCandy', fontSize: 11 }}
-              >
-                <ListItem>
-                  <TextNode>sub section 1</TextNode>
-                </ListItem>
-                <ListItem>
-                  <TextNode>sub section 2</TextNode>
-                </ListItem>
-              </List>
-            </ListItem>
-          </List>
-        </ListItem>
-      </List>
+          ))}
+        </List> */}
+      </View>
     </Page>
   );
 };
