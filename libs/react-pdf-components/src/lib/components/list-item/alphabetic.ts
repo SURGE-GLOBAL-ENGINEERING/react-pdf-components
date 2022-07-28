bba869292a42fc708a8cdcdf3b56138dc3f85de5
dalanad <dalana199981@gmail.com>
2022-07-28 11:05:08 +0530

    fix(list-item): fix alphebet based numbering

--- libs/react-pdf-components/src/lib/components/list-item/alphabetic.ts
+++ libs/react-pdf-components/src/lib/components/list-item/alphabetic.ts
@@ -1,3 +1,10 @@
+/**
+ * maps an integer to the alphabet based numbering
+ *   0  => a
+ *   1  => b
+ *   26 => aa
+ *   27 => ab
+ */
 export function arabToAlphabetic(n: number) {
   const charCodeA = 'a'.charCodeAt(0);
   const charCodeZ = 'z'.charCodeAt(0);
